cd827893aebaea43672d5874bf98f64302a61c34
Sakib Ahmed Sumdany <sakib.bracueee@gmail.com>
2024-04-08 11:20:28 +0000

    added ros2 auto sourcing and model path exports Changes to be committed: 	modified:   .devcontainer/post-create-command.sh

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -16,3 +16,9 @@ if [ -d "$ROOT_SRV" ]; then
     for dir in $OVERLAY_WS/src/navigation2/.devcontainer/caddy/srv/*; \
         do if [ -d "$dir" ]; then ln -s "$dir" $ROOT_SRV; fi done
 fi
+
+# ros2 Sourcing and model paths
+echo 'source /opt/ros/humble/setup.bash' >> ~/.bashrc
+echo 'export TURTLEBOT3_MODEL=waffle' >> ~/.bashrc
+echo 'export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:/opt/ros/humble/share/turtlebot3_gazebo/models' >> ~/.bashrc
+echo 'cd /opt/overlay_ws && source install/setup.bash && pwd' >> ~/.bashrc
